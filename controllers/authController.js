9fda7415a28e843c9eb82ae9ae05ca6883e51029
Adil <adil@example.com>
2025-06-10 16:10:43 +0530

    Fifth commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -97,4 +97,26 @@ export const loginController = async(req,res)=>{
         
     }
 }
+export const testController = (req,res)=>{
+    res.send("Protected Routes");
+}
 
+export const isAdmin = async (req,res,next)=>{
+    try{
+        const user = await userModel.findById(req.user._id)
+        if(user.role !==1){
+            return res.status(401).send({
+                success:false,
+                message:"UnAuthorized Access",
+            })
+        }
+        else{
+            next()
+        }
+        
+    }
+    catch(error){
+        console.log(error);
+        
+    }
+}
